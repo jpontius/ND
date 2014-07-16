8ccd7270d3f2266658adb89703f33a64418416be
Justin <justin.pontius@gmail.com>
2014-07-16 11:25:51 -0400

    fixed del_users

--- del_users.sh
+++ del_users.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-while read u; do
+while read user; do
 
 # Test that the NetID is the valid University NetID
 
-/bin/grep -w "$u" /afs/nd.edu/common/etc/passwd 
-if [ "$u" -ne 0 ]     
-   then 
-       echo "Error: NetID: <" $NetID "> is not valid. Please specify a valid Notre Dame NetID."
-       echo "$NetID" >> bad-NetID.txt
+/bin/grep -w "$user" /afs/nd.edu/common/etc/passwd
+if [ "$user" -ne 0 ]
+   then
+       echo "Error: NetID: <" $user "> is not valid. Please specify a valid Notre Dame NetID."
+       echo "$user" >> bad-NetID.txt
       exit
-fi       
+fi
 
 done < users.txt
