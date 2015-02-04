20d81e1d387aabdddb3b8b8a58eaf70c2c7412e3
Justin <justin.pontius@gmail.com>
2015-02-04 09:15:19 -0500

    updated bad_users to latest

--- User_management/bad_users_check.sh
+++ User_management/bad_users_check.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 cd /billing
-mv users_not_in_billing.txt users_not_in_billing.txt.old
+mv not_in_billing.txt not_in_billing.txt.old
 
 # Get current users in system
-system_users=$(cat /etc/passwd | cut -d ":" -f1)
+#cat /etc/passwd | cut -d ":" -f1 > system_users
+getent passwd | grep -vE '(nologin|false)$' | awk -F: -v min=1000 -v max=9999999 '{if(($3 >= min)&&($3 <= max)) print $1}' | sort -u > system_users
 
 # Get current user account list.
 wget -qO - http://www3.nd.edu/~jpontius/users/users.csv > users.csv
-user_list=$(cat users.csv | cut -d "," -f1)
+cat users.csv | cut -d "," -f1 > user_list
+
 
 # Outputs list of users not in billing 'users.csv'
-grep --invert-match -wFf "$user-list" "$system_users" > users_not_in_billing.txt
+grep --invert-match -wFf user_list system_users > not_in_billing.txt
 
 # Check if files are different.
-#file1=$(md5 users_not_in_billing.txt)
-#file2=$(md5 users_not_in_billing.txt.old)
+cmp --silent not_in_billing.txt not_in_billing.txt.old || /usr/local/bin/swaks --from nmr@nd.edu --to jpontius@nd.edu --h-Subject "Notification from $HOSTNAME" --body "Billing users are different from actual users." --attach-type "text/plain" --attach /billing/not_in_billing.txt -n --silent 3
 
-#if [ "$file1" == "$file2" ]
-#then
-#    echo "Files have the same content"
-#else
-#    echo "Files have NOT the same content"
-#fi
+#/usr/local/bin/swaks --from nmr@nd.edu --to jpontius@nd.edu --h-Subject "Notification from $HOSTNAME" --body "Billing users are different from actual users." --attach-type "text/plain" --attach /billing/not_in_billing.txt -n --silent 3
