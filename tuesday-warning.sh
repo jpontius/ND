3c37ee1400a4201efafb915bd4aa38186162c67f
Justin Pontius <justin.pontius@gmail.com>
2015-11-03 11:15:13 -0500

    added tuesday warning for diffusion probe

---
+++ tuesday-warning.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DAYOFWEEK=$(date +"%u")
+time=$(date +%k%M)
+
+#echo DAYOFWEEK: $DAYOFWEEK
+#echo Time: $time
+
+if [[ "$DAYOFWEEK" == 2 ]] && [[ "$time" -ge 900 ]] ;
+then
+   zenity --error --text "The diffusion probe is installed.\n \nThis probe is not suitable for normal experiments.\n \nPlease contact NMR staff for assistance."
+elif [[ "$DAYOFWEEK" == 3 ]] && [[ "$time" -le 900 ]] ;
+then
+  zenity --error --text "The diffusion probe is installed.\n \nThis probe is not suitable for normal NMR experiments.\n \nPlease contact NMR staff for assistance."
+else
+  exit 0
+fi
